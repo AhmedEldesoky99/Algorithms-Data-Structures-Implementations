a3107b121933fa01d9f1b07b45deb9aeb9fed11e
Ahmed Eldesoky <ahmedeldedoky99@gmail.com>
2023-10-27 16:59:45 +0200

    Bubble sort

--- Sort/Bubble Sort/BubbleSort.ts
+++ Sort/Bubble Sort/BubbleSort.ts
@@ -1,5 +1,22 @@
-function comparator(num1: number, num2: number): number {
-  return num1 - num2;
+function swap(arr: any[], index1: number, index2: number) {
+  let temp = arr[index1];
+  arr[index1] = arr[index2];
+  arr[index2] = temp;
 }
 
-console.log([7, 8, 9, 1].sort(comparator));
+function BubbleSortOptimized(arr: number[]): number[] {
+  let noSwap = true;
+  for (let i = arr.length; i > 0; i--) {
+    for (let j = 0; j < i - 1; j++) {
+      if (arr[j] > arr[j + 1]) {
+        swap(arr, j + 1, j);
+        noSwap = false;
+      }
+    }
+    if (noSwap) break;
+  }
+
+  return arr;
+}
+
+console.log(BubbleSortOptimized([10, -25, -6, 77, -88]));
