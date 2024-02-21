9776a66cd3ffffab9819d80773326432bb32c3db
AhmedEldesoky99 <ahmedeldedoky99@gmail.com>
2024-02-21 16:24:02 +0200

    fib Tabulation

--- Recurion/fib.ts
+++ Recurion/fib.ts
@@ -1,7 +1,7 @@
-// Memoization O(n)
+// Memoization O(n) but large space complexity
 const memo: { [name: string]: number } = {};
 
-function optFib(num: number, memo: { [name: string]: number }): number {
+function fibMemo(num: number, memo: { [name: string]: number }): number {
   if (memo[num]) return memo[num];
 
   if (num < 2) {
@@ -9,17 +9,30 @@ function optFib(num: number, memo: { [name: string]: number }): number {
     return num;
   }
 
-  let res = optFib(num - 1, memo) + optFib(num - 2, memo);
+  let res = fibMemo(num - 1, memo) + fibMemo(num - 2, memo);
 
   memo[num] = res;
 
   return res;
 }
 
-console.log(optFib(1000, memo));
+console.log(fibMemo(5, memo));
+
+//tabulation version
+function fibTabulation(num: number): number {
+  if (num <= 2) return 1;
+  let fibSeries = [0, 1, 1];
+
+  for (let i = 3; i <= num; i++) {
+    fibSeries[i] = fibSeries[i - 1] + fibSeries[i - 2];
+  }
+
+  return fibSeries[num];
+}
+console.log(fibTabulation(5));
 
 // O(2^n)
 function fib(num: number): number {
   return num < 2 ? num : fib(num - 1) + fib(num - 2);
 }
-console.log(fib(10));
+console.log(fib(5));
