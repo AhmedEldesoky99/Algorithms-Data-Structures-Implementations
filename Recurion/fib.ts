071ca8f4dffb16cf3bbaf544b8f9e5e9cecfcde0
AhmedEldesoky99 <ahmedeldedoky99@gmail.com>
2024-02-21 16:08:45 +0200

    Memoization Recursion

--- Recurion/fib.ts
+++ Recurion/fib.ts
@@ -1,5 +1,25 @@
-function fib(num: number): number {
-  return num === 0 || num === 1 ? num : fib(num - 1) + fib(num - 2);
+// Memoization O(n)
+const memo: { [name: string]: number } = {};
+
+function optFib(num: number, memo: { [name: string]: number }): number {
+  if (memo[num]) return memo[num];
+
+  if (num < 2) {
+    memo[num] = 1;
+    return num;
+  }
+
+  let res = optFib(num - 1, memo) + optFib(num - 2, memo);
+
+  memo[num] = res;
+
+  return res;
 }
 
-console.log(fib(3));
+console.log(optFib(1000, memo));
+
+// O(2^n)
+function fib(num: number): number {
+  return num < 2 ? num : fib(num - 1) + fib(num - 2);
+}
+console.log(fib(10));
